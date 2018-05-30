2237a66a5ac4e391def9b09ae088a5e1d48766be
chuck- <carlogreco93@gmail.com>
2018-05-30 13:53:14 +0200

    update example

--- example.py
+++ example.py
@@ -16,24 +16,36 @@ print('Public key: %s' % binascii.hexlify(bob.pubkey()))
 print('Balance: %f' % bob.balance())
 print('#' * 50)
 
-
-print(alice.balance())
-
-exit()
-
 from transaction import Transaction, TransactionInput, TransactionOutput
 from block import Block
 from dao import TransactionDAO
 
+coinbase = TransactionDAO.read_by_hash('2c3a24c14f3a2a017ee2591d4402b42443c96152a532713a11e1124dbabccc00')
+
+'''
+t = Transaction()
+# alice has an output in coinbase transaction, at index 0,
+# so she provides the signatures and public key to unlock that output
+t.addInput(TransactionInput(coinbase.checksum(), 0, alice.sign(coinbase.checksum()), alice.pubkey()))
+t.addOutput(TransactionOutput(10, bob.address()))
+t.addOutput(TransactionOutput(15, alice.address()))
+
+print(binascii.hexlify(t.checksum()))
+print(t.verify())
+
+'''
+
 # the first coinbase transaction (from genesis block)
-coinbase = TransactionDAO.read_by_hash('093c101a53e43e25893f04631e70ea86a64628ba982e85bf59d8ebe3be38fd0f')
+coinbase = TransactionDAO.read_by_hash('2c3a24c14f3a2a017ee2591d4402b42443c96152a532713a11e1124dbabccc00')
 
 # let Alice send some value to Bob
-#t = Transaction()
-#t.addInput(TransactionInput(coinbase.checksum(), 0, alice.sign(coinbase.checksum()), alice.pubkey()))
-#t.addOutput(TransactionOutput(10, bob.address()))
-#t.addOutput(TransactionOutput(15, alice.address()))
-t = TransactionDAO.read_by_hash('985777eac46108b05259e54037efd996b6c052562f61ddc85acd9698763b499b')
+t = Transaction()
+# alice has an output in coinbase transaction, at index 0,
+# so she provides the signatures and public key to unlock that output
+t.addInput(TransactionInput(coinbase.checksum(), 0, alice.sign(coinbase.checksum()), alice.pubkey()))
+t.addOutput(TransactionOutput(10, bob.address()))
+t.addOutput(TransactionOutput(15, alice.address()))
+#t = TransactionDAO.read_by_hash('985777eac46108b05259e54037efd996b6c052562f61ddc85acd9698763b499b')
 
 
 # send the transaction to the daemon
@@ -41,15 +53,14 @@ import socket
 import sys
 
 HOST, PORT = 'localhost', 9999
-
-# Create a socket (SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
 	st = t.serialize().encode()
 	
 	sock.connect((HOST, PORT))
-	sock.send(len(st).to_bytes(4, byteorder='big'))	
+	sock.sendall(len(st).to_bytes(4, byteorder='big'))	
 	sock.sendall(t.serialize().encode())
 finally:
 	sock.close()
+#'''
